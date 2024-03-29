491357e9603f119969e91b16f3446c0a8a0b75b3
l1monc3llo <110810713+l1monc3llo@users.noreply.github.com>
2024-03-29 11:20:33 +0300

    Update 997. Find The Town Judge.java

--- 997. Find The Town Judge.java
+++ 997. Find The Town Judge.java
@@ -1,21 +1,20 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[][] inmatrix = new int[n][2]; // Assuming each person is represented by an index from 0 to n-1
-        int[][] outmatrix = new int[n][2]; // Assuming each person is represented by an index from 0 to n-1
+        HashMap<Integer, Integer> trustDelta = new HashMap<>();
 
         for (int i = 0; i < trust.length; ++i) {
-            int trustingPerson = trust[i][0] - 1; // Adjust index to start from 0
-            int trustedPerson = trust[i][1] - 1; // Adjust index to start from 0
-            inmatrix[trustedPerson][1]++;
-            outmatrix[trustingPerson][0]++;
+            int trustee = trust[i][0];
+            int trusted = trust[i][1];
+            trustDelta.put(trustee, trustDelta.getOrDefault(trustee,0) + 1);
+            trustDelta.put(trusted, trustDelta.getOrDefault(trusted,0) - 1);
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (inmatrix[i][1] == n - 1 && outmatrix[i][0] == 0) {
-                return i + 1; // Adjust index to start from 1
+        for (int i = 1; i < n+1; ++i) {
+            if (trustDelta.getOrDefault(i, 0) == -(n - 1)) {
+                return i;
             }
         }
 
-        return -1; // No judge found
+        return -1;
     }
 }
